63f3f7c498a71fb890f6656d58a4b905934d9ebc
phillipdunn <phillipdunn@hotmail.com>
2022-02-16 16:08:14 +0000

    render todos on page

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const todoList = document.getElementById('todo-list');
+
 const getTodos = () => {
   fetch('http://localhost:3000/todos')
     .then((res) => res.json())
-    .then((res) => console.log(res));
+    .then((res) => renderTodos(res));
+};
+
+const renderTodos = (todos) => {
+  const todosList = todos.map((todo) => {
+    return `<li>${todo.title} </li>`;
+  });
+  todoList.innerHTML = todosList.join('');
 };
 
 getTodos();
