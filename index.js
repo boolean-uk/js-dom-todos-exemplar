0ede6d8da98558061da24da9bb284cbdf5dcce64
Carlo Minciacchi <carlo.minciacchi@leapian.com>
2022-10-28 11:39:26 +0100

    Implemented objective one - getting list of todos

--- index.js
+++ index.js
@@ -1,99 +1,86 @@
-const list = document.querySelector(".todo-list")
-const form = document.querySelector(".todo-form")
-const errorElement = document.querySelector(".error")
+// SELECT HTML ELEMENTS SO WE CAN WRITE TO THE PAGE
+const list = document.querySelector(".todo-list");
+const form = document.querySelector(".todo-form");
+const errorElement = document.querySelector(".error");
 
-// Make a GET request with fetch to http://localhost:3000/todos to load all Todos from the server and render them in a list. Completed Todos should be grey and scored out.
+// WORKSHOP OBJECTIVES
+// Review how to organise and plan our work
+// as we get started
+// Demonstrate code to set up LOCAL STATE
+// Review FETCH
 
-function getAndRenderTodoList() {
-  fetch("http://localhost:3000/todos")
-    .then(res => res.json())
-    .then(data => {
-      renderTodoList(data)
-    })
-    .catch(e => {
-      console.log(e)
-      errorElement.innerText = "SORRY, NO LUCK TODAY!"
-    })
-}
-function renderTodoList(todos) {
-  // clear the html
-  list.innerHTML = ""
-  // loop through the todo items
-  todos.forEach(todo => {
-    // create the todo list item element
-    const li = createTodoListItem(todo)
-    // append to the list
-    list.append(li)
-  })
-}
+// OBJ 1
+// Make a GET request with fetch to http://localhost:3000/todos to
+// load all Todos from the server and render them in a list.
+// Completed Todos should be grey and scored out.
 
-function createTodoListItem(todo) {
-  const li = document.createElement("li")
+// OBJ 2
+// when user clicks add, POST a new todo item
+// option 1: detect form submit event; prevent default behaviour; send fetch POST reuqest for a new todo
+// when repsonse comes back, add todo to state.todods and re-render list
+// option 2: detect form input events and store the title of the new todo in state
+// then continue with option 1
 
-  // Add a "Complete" button to each uncompleted Todo. When the user clicks it, make a PATCH request with fetch to http://localhost:3000/todos/[todoid] (replacing todoid with the actual ID of the todo) to update the todo as completed. Update the list of Todos without reloading the page.
-  const completeButton = document.createElement("button")
-  completeButton.innerText = "complete"
-  completeButton.addEventListener("click", event => {
-    // send a PATCH request
-    const obj = {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({completed: true})
-    }
-    fetch(`http://localhost:3000/todos/${todo.id}`, obj)
-      .then(res => res.json())
-      .then(data => {
-        // rerender the list
-        getAndRenderTodoList()
-      })
-    })
+// STATE
+const state = {
+  todos: [],
+};
+// HANDLE ACTIONS CODE
 
-// Add a "Delete" button to each Todo. When the user clicks it, make a DELETE request with fetch to http://localhost:3000/todos/[todoid] (replacing todoid with the actual ID of the todo) to remove the todo. Update the list of Todos without reloading the page.
-  const deleteButton = document.createElement("button")
-  deleteButton.innerText = "delete"
-  deleteButton.addEventListener("click", event => {
-    // send a DELETE request
-    const obj = {
-      method: "DELETE",
-    }
-    fetch(`http://localhost:3000/todos/${todo.id}`, obj)
-      .then(res => res.json())
-      .then(data => {
-        // rerender the list
-        getAndRenderTodoList()
-      })
-    })
+// RENDER CODE
+function createTodoLI(todo) {
+  const li = document.createElement("li");
+  li.innerText = todo.title;
 
-  li.innerText = todo.title
-  li.append(deleteButton)
-
-  if (todo.completed) {
-    li.setAttribute("class", "completed")
-  } else {
-    li.append(completeButton)
+  // if todo.complted === true, we strikethrough
+  if (todo.completed === true) {
+    li.setAttribute("class", "completed");
   }
 
-  return li
+  return li;
 }
 
-// When the form is submitted, make POST request with fetch to http://localhost:3000/todos to create a new Todo. Update the list of Todos without reloading the page.
-form.addEventListener("submit", event => {
-  // prevent page reload
-  event.preventDefault()
-  // get value from input field
-  const title = event.target[0].value
-  // make a POST request with data
-  const obj = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({title: title, completed: false})
-  }
-  fetch(`http://localhost:3000/todos`, obj)
-    .then(res => res.json())
-    .then(data => {
-      getAndRenderTodoList()
+function renderAllTodos() {
+  // 3) when LOCAL state is updated, call a function to render the TODOS
+  // a) clear the original list of todos
+  list.innerHTML = "";
+  // b) for each todo, I want to render a new todo
+  state.todos.forEach((todo) => {
+    // c) create a function that creates one HTML <li> for each todo
+    const li = createTodoLI(todo);
+    // d) add li to the list <ul>
+    list.appendChild(li);
+  });
+}
+
+// INIT - the first thing that is always run, when your page first loads
+// 1) when page loads, call a function that makes a GET request for all TODOS
+// fetch http://localhost:3000/todos with GET
+function getAllTodosFromServer() {
+  // this is a GET, no need to pass OPTIONS to fetch
+  // call the fetch function
+  fetch("http://localhost:4000/todos")
+    // when I get a response, run the following 2 functions
+    .then((responseFromServer) => {
+      // check if my response was problematic:
+      if (responseFromServer.status !== 200)
+        throw Error("There was some problem GETting all Todos");
+      // extracts the JSON data from the response and returns it from this function
+      // the JSON data is returned as JS arrays and JS objects
+      // what we return here becomes the parameter for the next then() function
+      return responseFromServer.json();
     })
-  })
+    .then((arrayOfTodos) => {
+      // 2) on response from GET, update LOCAL state with the todos
+      // state.todos is an array of all todos from the server
+      state.todos = arrayOfTodos;
+      // re-render my todo list
+      renderAllTodos();
+    })
+    .catch((error) => {
+      console.log("ERROR", error);
+      errorElement.innerText = error.message;
+    });
+}
 
-
-getAndRenderTodoList()
+getAllTodosFromServer();
